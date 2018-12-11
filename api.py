5b3365648cc43832638ef997eaf8cea52a609708
jstricker21 <43145298+jstricker21@users.noreply.github.com>
2018-12-11 10:15:47 -0500

    Update api.py

--- api.py
+++ api.py
@@ -68,12 +68,23 @@ def delete_player(username):
     players = db.getPlayers()
     for player in players:
         if player.username == username and player.password == password:
-
+            db.deletePlayer(player)
+            db.commit()
+        elif player.username == username and player.passowrd != password:
+            abort (403, "Wrong password")
     abort (404, "No Player")
 
 @app.route('/<username>/<gameID>', methods = ['GET'])
 def get_game(username, gameID):
-    pass
+    players = db.getPlayers()
+    for player in players:
+        if username == player.username:
+            for game in player.games:
+                if game.id == gameID:
+                    wanted_game = {'id':game.id, 'link': "/"+game.id, 'player score' : game.player_score, 'oppenent score' : game.other_score}
+                    return make_json_response(wanted_game, 200)
+            abort(403, "Game does not exist")
+    abort(404,'Person does not exist')
 
 @app.route('/<username>', methods = ['POST'])
 def create_game(username):
@@ -81,7 +92,21 @@ def create_game(username):
 
 @app.route('/<username>/<gameID>', methods = ['DELETE'])
 def delete_game(username, gameID):
-    pass
+    def delete_player(username):
+        password = request.args.get('password')
+        if password == None:
+           abort(403, 'must provide a password')
+        players = db.getPlayers()
+        for player in players:
+            if player.username == username and player.password == password:
+                for game in player.games:
+                    if game.id == gameID:
+                        db.deleteGame(game)
+                        db.commit()
+                abort(403, "Game does not exist")
+            elif player.username == username and player.passowrd != password:
+                abort (403, "Wrong password")
+        abort (404, "Player does not exist")
 
 # Starts the application
 if __name__ == "__main__":
